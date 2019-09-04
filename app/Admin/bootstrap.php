21becf845912d02e9b821ac06ecbbdaf6b03c378
chenyi <wb-chenyi@ibantang.com>
2019-09-04 15:42:54 +0800

    第一版

---
+++ app/Admin/bootstrap.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Laravel-admin - admin builder based on Laravel.
+ * @author z-song <https://github.com/z-song>
+ *
+ * Bootstraper for Admin.
+ *
+ * Here you can remove builtin form field:
+ * Encore\Admin\Form::forget(['map', 'editor']);
+ *
+ * Or extend custom form field:
+ * Encore\Admin\Form::extend('php', PHPEditor::class);
+ *
+ * Or require js and css assets:
+ * Admin::css('/packages/prettydocs/css/styles.css');
+ * Admin::js('/packages/prettydocs/js/main.js');
+ *
+ */
+
+Encore\Admin\Form::forget(['map', 'editor']);
+
+app('view')->prependNamespace('admin', resource_path('views/admin'));
+
+Encore\Admin\Form::init(function ($form) {
+    $form->tools(function ($tools) {
+        $tools->disableDelete();
+        $tools->disableView();
+        $tools->disableList();
+    });
+});
